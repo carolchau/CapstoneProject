a105bab744658154b71d8a1aa3f21dfc79137afc
Maximilian Ioffe <maxioffe@gmail.com>
2018-04-13 14:44:06 -0400

    Readd files

---
+++ config/database.js
@@ -0,0 +1,1 @@
+module.exports = {'url' : 'mongodb://localhost:27017/test'};
---
+++ models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// Define schema for user model
+var userSchema = mongoose.Schema({
+  local : {
+    email : String,
+    password : String,
+  }
+});
+
+// Generate hash
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// Check if password is valid
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+// Create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
