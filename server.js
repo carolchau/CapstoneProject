7b78e0dbc04c4a098fcb90ccd0ff546dd7fabe29
Brian Hernandez <Brian@Hernandez-cs.com>
2018-05-14 03:28:03 -0400

    first implementation of hat generation and collision

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const WebSocket = require('ws');
 const { spawn } = require('child_process');
 const WORLD_UNIT = 16;
 const WORLD_SIZE = 4000;
+const HAT_TYPES = 3;
+const HAT_COUNT = 1000;
+const CELL_SIZE = 32;
 
 var port = process.env.PORT || 3000;
 const server = http.createServer(app);
@@ -15,7 +18,6 @@ const wss = new WebSocket.Server({ server });
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
-//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
@@ -26,9 +28,6 @@ mongoose.connect(configDB.url, { useMongoClient: true });
 
 // Routes
 require('./config/passport')(passport);
-
-// Set up Express App
-//app.use(logger('combined'));
 
 // BodyParser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -57,22 +56,97 @@ app.use(flash());
 // Load Routes
 require('./routes/routes.js')(app, passport);
 
-//Variable to keep track of unique player indices
-var unique_counter = 0;
+// Hat distribution generation
+var hat_data = {
+    type: "hat_data",
+    data: []
+}
 
+// Initial hat hash variables
+var hat_hash = {};
+var unique_hat_id_counter = 0;
+
+// Generating hat objects
+let remaining_count = HAT_COUNT;
+for(let i = 0; i < HAT_TYPES-1; i++){
+    let slice_count = Math.floor(Math.random() * (remaining_count - (HAT_TYPES - i - 1))) + 1;
+    console.log("slice: " + slice_count);
+    for(let j = 0; j < slice_count; j++){
+        //Choose location
+        let width = 16;
+        let height = 16;
+        let temp_x = Math.floor(Math.random() * WORLD_SIZE * WORLD_UNIT - width);
+        let temp_y = Math.floor(Math.random() * WORLD_SIZE * WORLD_UNIT - height);
+        let temp_hx = Math.floor(temp_x/CELL_SIZE);
+        let temp_hy = Math.floor(temp_y/CELL_SIZE);
+
+        //Generate object
+        if(hat_hash[[temp_hx,temp_hy] == undefined]){
+            hat_hash[[temp_hx,temp_hy]] = [];
+        }
+        hat_hash[[temp_hx,temp_hy]].push(unique_hat_id_counter);
+        hat_data["data"].push([temp_x, temp_y, width, height]);
+        unique_hat_id_counter++;
+    }
+    remaining_count = remaining_count - slice_count;
+}
+for(let i = 0; i < remaining_count; i++){
+    //Choose location
+    let width = 16;
+    let height = 16;
+    let temp_x = Math.floor(Math.random() * WORLD_SIZE * WORLD_UNIT - width);
+    let temp_y = Math.floor(Math.random() * WORLD_SIZE * WORLD_UNIT - height);
+    let temp_hx = Math.floor(temp_x/CELL_SIZE);
+    let temp_hy = Math.floor(temp_y/CELL_SIZE);
+
+    //Generate object
+    if(hat_hash[[temp_hx,temp_hy] == undefined]){
+        hat_hash[[temp_hx,temp_hy]] = [];
+    }
+    hat_hash[[temp_hx,temp_hy]].push(unique_hat_id_counter);
+    hat_data["data"].push([temp_x, temp_y, width, height]);
+    unique_hat_id_counter++;
+}
+
+
+// Broadcast world data to all players at a set interval
 setInterval(function() {
-	var data = {
+	let data = {
 		type: "world_data",
     data: {}
 	};
 	for (let client of wss.clients) {
 		data["data"][client.unique_id] = {
 			x_position: client.x_position,
-		  y_position: client.y_position
+		  y_position: client.y_position,
+          collected: []
 		};
+        let hx_position = Math.floor(client.x_position/CELL_SIZE);
+        let hy_position = Math.floor(client.y_position/CELL_SIZE);
+        let candidate_length = hat_hash[[hx_position,hy_position]].length;
+        for(let i = 0; i < candidate_length; i++){
+            let hat_id = hat_hash[[hx_position,hy_position]][i];
+            let hat_pos = hat_data["data"][hat_id];
+            let player_left = client.x_position;
+            let player_right = player_left + client.width;
+            let player_top = client.y_position;
+            let player_bot = player_top + client.height;
+            let hat_left = hat_pos[0];
+            let hat_right = hat_left + hat_pos[2];
+            let hat_top = hat_pos[1];
+            let hat_bot = hat_top + hat_pos[3];
+            if(player_top < hat_bot && player_right > hat_left &&
+               player_bot > hat_top && player_left > hat_right){
+                data["data"][client.unique_id]["collected"].push(hat_id); 
+            }
+        }
 	}
 	wss.broadcast(JSON.stringify(data));
 }, 50);
+
+
+// Variable to keep track of unique player indices
+var unique_counter = 0;
 
 // Listen in on every WebSocket connection
 wss.on('connection', (client) => {
